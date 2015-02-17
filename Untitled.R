046ba001e6c81617f8e36d5a99ca264ecac464fa
kenncheng <admin@imac.gateway.2wire.net>
2015-02-17 20:53:56 +0800

    PML project finalized
    final results of PML project updated

--- Untitled.R
+++
@@ -1,3 +0,0 @@
-# Read training data
-pml.training <- read.csv("~/desktop/coursera/Machine_Learning/pml-training.csv")
-pml.testing <- read.csv("~/desktop/coursera/Machine_Learning/pml-testing.csv")
